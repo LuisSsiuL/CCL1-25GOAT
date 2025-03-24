c93890b0df95e1a4fd672714da764719fee7b41a
LuisSsiuL <christian.luis.efendy@gmail.com>
2025-03-24 16:30:09 +0700

    Initial Commit

---
+++ CCL1-25GOAT/CCL1_25GOATApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCL1_25GOATApp.swift
+//  CCL1-25GOAT
+//
+//  Created by Christian Luis Efendy on 24/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CCL1_25GOATApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
