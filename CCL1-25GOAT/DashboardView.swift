a6ccee7ea5164b31e9cffdc51b8330a3c72c40ea
kennethmyrwjy <145191763+kennethmyrwjy@users.noreply.github.com>
2025-03-25 16:28:10 +0700

    initial dashboard design, native search bar version

---
+++ CCL1-25GOAT/DashboardView.swift
@@ -0,0 +1,8 @@
+//
+//  DashboardView.swift
+//  CCL1-25GOAT
+//
+//  Created by Kenneth Mayer on 24/03/25.
+//
+
+import Foundation
---
+++ CCL1-25GOAT/VehicleDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  VehicleDetailView.swift
+//  CCL1-25GOAT
+//
+//  Created by Kenneth Mayer on 25/03/25.
+//
+
+import Foundation
