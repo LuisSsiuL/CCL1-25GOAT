8ca3b6ec61b19c8dc2ef03f0313c85f308d059e8
kennethmyrwjy <145191763+kennethmyrwjy@users.noreply.github.com>
2025-03-25 16:30:10 +0700

    initial dashboard design, native search bar ver.

--- CCL1-25GOAT/VehicleDetailView.swift
+++ CCL1-25GOAT/VehicleDetailView.swift
@@ -1,8 +1,43 @@
-//
-//  VehicleDetailView.swift
-//  CCL1-25GOAT
-//
-//  Created by Kenneth Mayer on 25/03/25.
-//
+import SwiftUI
 
-import Foundation
+struct VehicleDetailView : View {
+    
+    let plate: String
+    
+    var body: some View {
+            
+        VStack {
+            Text("Vehicle Detail View").font(.headline)
+        }.background(.ultraThinMaterial)
+            .navigationBarTitleDisplayMode(.inline) // gets rid of spacing below title
+        // need to change to actual background color
+        .toolbar {
+            ToolbarItem(placement: .principal) {
+                Button {
+                    
+                } label: {
+                    HStack {
+                        Text(plate).font(.system(size: 20, weight: .bold, design: .default)).foregroundStyle(.white)
+                        Image(systemName: "pencil")
+                    }
+                }
+            }
+            
+            ToolbarItem(placement: .bottomBar) {
+                Button {
+                    
+                } label: {
+                    Text("+ Add New Note").font(.system(size: 19, weight: .bold, design: .default))
+                        .foregroundStyle(.white)
+                        .frame(width: 330, height: 36, alignment: .center)
+                        .background(Color.blue)
+                        .containerShape(RoundedRectangle(cornerSize: .init(width: 10, height: 10)))
+                }
+            }
+        }.toolbarBackground(.visible, for: .navigationBar)
+    }
+}
+
+#Preview {
+    DashboardView()
+}
